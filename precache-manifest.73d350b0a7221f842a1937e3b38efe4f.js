5200a97a10f07280472192bd6b861012071fa5bc
MaryamGhods <maryam.gh1500@gmail.com>
2022-07-14 15:56:12 +0330

    Updates

---
+++ precache-manifest.73d350b0a7221f842a1937e3b38efe4f.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5a57d11d5450b886513",
+    "url": "/personal-website/static/css/main.6dd8bc79.chunk.css"
+  },
+  {
+    "revision": "a5a57d11d5450b886513",
+    "url": "/personal-website/static/js/main.a5a57d11.chunk.js"
+  },
+  {
+    "revision": "4bb05d8fed1e4d6e8559",
+    "url": "/personal-website/static/js/1.4bb05d8f.chunk.js"
+  },
+  {
+    "revision": "62f5bc6b9e4ba2d8cf70",
+    "url": "/personal-website/static/css/2.9c16861e.chunk.css"
+  },
+  {
+    "revision": "62f5bc6b9e4ba2d8cf70",
+    "url": "/personal-website/static/js/2.62f5bc6b.chunk.js"
+  },
+  {
+    "revision": "b6cf602714a4a7c41406",
+    "url": "/personal-website/static/js/runtime~main.b6cf6027.js"
+  },
+  {
+    "revision": "790d59ce48845bdb8ed9f78dafacdca8",
+    "url": "/personal-website/static/media/Landing Page.790d59ce.png"
+  },
+  {
+    "revision": "25916c96908196070010c9eff0b869f2",
+    "url": "/personal-website/static/media/Credit Card Checkout.25916c96.png"
+  },
+  {
+    "revision": "a2f52436d6f139830a2f7cb034e40f28",
+    "url": "/personal-website/static/media/Sign Up.a2f52436.png"
+  },
+  {
+    "revision": "abb247ff1223bff825c8be8619cb83fb",
+    "url": "/personal-website/static/media/Calculator.abb247ff.png"
+  },
+  {
+    "revision": "d2ce259cacf561a5c98e3cf42a763629",
+    "url": "/personal-website/static/media/laptop3.d2ce259c.jpg"
+  },
+  {
+    "revision": "bd7b29f82a839b69985bc9a39aea12a5",
+    "url": "/personal-website/static/media/colorful-background.bd7b29f8.jfif"
+  },
+  {
+    "revision": "6432364809c324199b9297576e6eafa0",
+    "url": "/personal-website/static/media/colorful-background2.64323648.jfif"
+  },
+  {
+    "revision": "e0ebc00f4f332f2b0d1ea40992579a44",
+    "url": "/personal-website/static/media/colorful-background3.e0ebc00f.jfif"
+  },
+  {
+    "revision": "eae662b83e40fddef47761434cc0fb37",
+    "url": "/personal-website/static/media/bnazanin-webfont.eae662b8.woff"
+  },
+  {
+    "revision": "eea61f7f24e3bdc49fd1d24d3ba105b6",
+    "url": "/personal-website/static/media/bnazanin-webfont.eea61f7f.woff2"
+  },
+  {
+    "revision": "0313a623c0b92aa028ae2c4018ad2909",
+    "url": "/personal-website/static/media/laptop1.0313a623.png"
+  },
+  {
+    "revision": "51c9bf4505a851b12b8ebf3327723f8d",
+    "url": "/personal-website/static/media/Watercolor-rb.51c9bf45.png"
+  },
+  {
+    "revision": "9d80cf18f72547652f11998911d820d3",
+    "url": "/personal-website/static/media/personal2.9d80cf18.png"
+  },
+  {
+    "revision": "1ac7bf4a0efb7f8080b49af667004017",
+    "url": "/personal-website/static/media/personal.1ac7bf4a.png"
+  },
+  {
+    "revision": "4627bb3882cbd4ebd594d8da51a060a8",
+    "url": "/personal-website/static/media/laptop2.4627bb38.jpg"
+  },
+  {
+    "revision": "c94dd787dd3b63360924552f5c990d32",
+    "url": "/personal-website/static/media/teaching.c94dd787.png"
+  },
+  {
+    "revision": "335e60e0057674f94e2a7bba105edf67",
+    "url": "/personal-website/static/media/designing.335e60e0.png"
+  },
+  {
+    "revision": "49340e22721dbf560567a9e847fbdfd5",
+    "url": "/personal-website/static/media/coding.49340e22.png"
+  },
+  {
+    "revision": "f4abe3f8afd684ce35c08071ef8652d6",
+    "url": "/personal-website/static/media/flower.f4abe3f8.png"
+  },
+  {
+    "revision": "56f5cfccefc4e8f0cd3fd259fdf01ef1",
+    "url": "/personal-website/static/media/Moon.56f5cfcc.png"
+  },
+  {
+    "revision": "0af68e093b7b10c499a9a24f45e58a39",
+    "url": "/personal-website/index.html"
+  }
+];
